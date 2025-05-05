6fb4fb084ce10c6861b28fab7a06b61c7e32c77d
LewisJassy <jassylewis70@gmail.com>
2025-05-05 11:17:06 +0300

    chore: Update Django settings to use environment variables for sensitive data and switch to PostgreSQL database

--- chatbot_backend/chatbot_backend/settings.py
+++ chatbot_backend/chatbot_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#w-75*=bjt3%x6%-u!g7$eoruayis6auwdf%!s^n5&fbqekr))'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,10 +156,15 @@ WSGI_APPLICATION = 'chatbot_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
+
 
 
 # Password validation
@@ -201,6 +211,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authorization and Authentication
 AUTH_USER_MODEL = 'chatbot.CustomUser'
+
+CHAT_HISTORY_MODEL = 'chatbot.ChatHistory'
 
 # Session settings
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
