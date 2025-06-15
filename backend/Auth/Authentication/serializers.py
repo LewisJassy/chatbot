3effddb5cc42cde16e0aabed844e28f4b3009f05
LewisJassy <jassylewis70@gmail.com>
2025-06-15 13:25:31 +0300

    fix: Remove unnecessary blank line in serializers.py

--- backend/Auth/Authentication/serializers.py
+++ backend/Auth/Authentication/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
+
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
